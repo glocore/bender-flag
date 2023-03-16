c1303050695f1060f9bcf2d8aefb223e4db2cd60
glocore <mailtoashwinm@gmail.com>
2023-03-16 17:50:00 +0530

    i fucked up

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -1,4 +1,12 @@
-void main()
-{
-    gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
+uniform vec3 uColor;
+uniform sampler2D uTexture;
+
+varying vec2 vUv;
+varying float vElevation;
+
+void main() {
+  vec4 textureColor = texture2D(uTexture, vUv);
+  textureColor.rgb *= vElevation * 2.0 + 1.0;
+  
+  gl_FragColor = textureColor;
 }
